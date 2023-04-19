065c7d491c8da39061cb84fbd83a353ad694921b
ahmad <ahmadhamsa01@gmail.com>
2023-04-19 15:50:44 +0800

    19 April 2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './containers/home';
+import Skills from './containers/skills';
+import About from './containers/about';
+import Resume from './containers/resume';
+import Contact from './containers/contact';
+import Portfolio from './containers/portfolio';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+    {/* particles js */}
+
+    {/* navbar */}
+
+    {/* main page content */}
+    <Routes>
+        <Route index path='/' element={<Home/>}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/resume' element={<Resume />}/>
+        <Route path='/skills' element={<Skills />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/portfolio' element={<Portfolio />}/>
+    </Routes>
+
+</div>
+);
 }
 
 export default App;
