24b3dcd55b627e450b6ddd172dc20a88d58c757d
ahmad <ahmadhamsa01@gmail.com>
2023-04-24 14:21:23 +0800

    About page

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,15 +1,112 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeader";
-import {BsInfoCircleFill} from 'react-icons/bs';
+import { BsInfoCircleFill } from "react-icons/bs";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
 
-const About=()=>{
-    return(
-        <section id="about" className="about">
-            <PageHeaderContent
-                headerText = "About Me"
-                icon = {<BsInfoCircleFill size={40} />}
-            />
-        </section>
-    );
+const jobSummary =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Ahmad Hamsa Pattuneri",
+  },
+  {
+    label: "Age",
+    value: "21",
+  },
+  {
+    label: "Address",
+    value: "Indonesia",
+  },
+  {
+    label: "Email",
+    value: "ahmadhamsa01@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+62895360674654",
+  },
+];
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Front End Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Personal Information</h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(775px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var(--yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var(--yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var(--yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var(--yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default About;
