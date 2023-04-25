8bf5639d626da2ed65de72b63d945f96a5b14bd6
ahmad <ahmadhamsa01@gmail.com>
2023-04-25 23:36:50 +0800

    Skills page

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,73 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "50",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "80",
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "80",
+            },
+            {
+                skillName: "ANGULAR",
+                percentage: "80",
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "JAVA",
+                percentage: "80",
+            },
+            {
+                skillName: "PYTHON",
+                percentage: "80",
+            },
+            {
+                skillName: "RUBY",
+                percentage: "80",
+            },
+            {
+                skillName: "C++",
+                percentage: "80",
+            },
+            {
+                skillName: "KOTLIN",
+                percentage: "80",
+            },
+        ],
+    },
+    {
+        label : "DATABASES",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentage: "80",
+            },
+            {
+                skillName: "MONGODB",
+                percentage: "80",
+            },
+            {
+                skillName: "REDIS",
+                percentage: "80",
+            },
+            {
+                skillName: "ORACLE",
+                percentage: "80",
+            },
+        ],
+    },
+]
