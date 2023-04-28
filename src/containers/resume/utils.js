e2b69db2840a86e3496b1a22a2590dbf79d910ff
ahmad <ahmadhamsa01@gmail.com>
2023-04-28 23:14:21 +0800

    Resume page - fiinishing

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,35 @@
+export const data = {
+    experience: [
+        {
+            title: "Consultant",
+            subTitle: "Miami, FL",
+            description: "User Experience, Visual Design",
+        },
+        {
+            title: "Senior Consultant",
+            subTitle: "London, UK",
+            description: "Web Development, Databases, Backend development   ",
+        },
+        {
+            title: "Project Manajer",
+            subTitle: "Bangalore, India",
+            description: "Creative Direction Project Management, Team Leading",
+        },
+    ],
+    education: [
+        {
+            title: "Secondary",
+            subTitle: "ABC School",
+            description: "10th Grade",
+        },
+        {
+            title: "Higher Secondary",
+            subTitle: "ABC School of higher education",
+            description: "12th Grade in Science",
+        },{
+            title: "B Tech XYZ Institute of Technology",
+            subTitle: "Bachelor of Computer Science",
+            description: "User Experience, Visual Design",
+        },
+    ],
+};
